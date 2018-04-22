ead361e45abfe04cf2f69ed35303e382448b1fe0
Rishi Kaushik <rishikaushik60@gmail.com>
2018-04-22 13:19:17 -0800

    added basic dijkstra search

--- graph.py
+++ graph.py
@@ -1,12 +1,20 @@
+import copy
+
 class connections:
     def __init__(self, target_node, distance):
         self.target_node = target_node
         self.distance = distance
 
+
 class node:
     def __init__(self, lable=0):
         self.lable = lable
         self.connections = []
+        self.traveled = 0
+        self.visited = False
+
+    # def __cmp__(self, other):
+    #     return cmp(self.traveled,other.traveled)
 
     def draw(self):
         print(self.lable, end='    ->   ')
@@ -18,6 +26,7 @@ class node:
 class graph:
     def __init__(self):
         self.nodes = []
+
 
     def add_node(self, lable=0):
         self.nodes.append(node(lable))
@@ -33,8 +42,8 @@ class graph:
             if node2 == node1.connections[i].target_node:
                 flag = False
         if node1 != node2 and flag:
-            node1.connections.append(connections(node2,distance))
-            node2.connections.append(connections(node1,distance))
+            node1.connections.append(connections(node2, distance))
+            node2.connections.append(connections(node1, distance))
 
     # def make_connections(self, current_node: node, node_list: list):
     #     for i in range(len(node_list)):
@@ -44,18 +53,38 @@ class graph:
         for i in range(len(self.nodes)):
             self.nodes[i].draw()
 
-    # def dijkstra(self,fir_node :node, sec_node:node):
+def getKey(node:node):
+    return node.traveled
+
+def dijkstra(start_node :node, finish_node:node):
+    priority_que = [start_node]
+    start_node.visited = True
+    while True:
+        priority_que.sort(key=getKey)
+        current = priority_que[0]
+        if current is finish_node:
+            return current.traveled
+        priority_que.pop(0)
+        for i in range(len(current.connections)):
+            if not current.connections[i].target_node.visited:
+                current.connections[i].target_node.traveled = current.traveled + current.connections[i].distance
+                priority_que.append(current.connections[i].target_node)
+                current.connections[i].target_node.visited = True
+            else:
+                current.connections[i].target_node.traveled = min(current.traveled + current.connections[i].distance, current.connections[i].target_node.traveled)
 
 
 
 g = graph()
+g.add_node(1)
+g.add_node(2)
+g.add_node(3)
+g.add_node(5)
 g.add_node(4)
-g.add_node(55)
-g.add_node(12)
-g.add_node(78)
-g.make_connection(g.get_node(4), g.get_node(12), 3)
-g.make_connection(g.get_node(55), g.get_node(12), 5)
-g.make_connection(g.get_node(78), g.get_node(12), 4)
-g.draw()
-
-
+g.make_connection(g.get_node(1), g.get_node(2), 10)
+g.make_connection(g.get_node(2), g.get_node(4), 2)
+g.make_connection(g.get_node(1), g.get_node(3), 30)
+g.make_connection(g.get_node(3), g.get_node(4), 4)
+g.make_connection(g.get_node(1), g.get_node(5), 5)
+g.make_connection(g.get_node(5), g.get_node(4), 6)
+print(dijkstra(g.get_node(1), g.get_node(4)))
